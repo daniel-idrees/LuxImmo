e37949e770fec4403786f4a67f12e2499d96ac84
daniyal <enggdaniyal@hotmail.com>
2026-01-15 05:47:23 +0100

    data: data module set to public for dependencies availability

--- core/data/src/main/kotlin/com/example/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/example/data/di/DataModule.kt
@@ -6,12 +6,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
     @Binds
-    internal abstract fun bindsListingRepository(
+    @Singleton
+    abstract fun bindsListingRepository(
         listingRepository: OfflineFirstListingRepository,
     ): ListingRepository
 }
