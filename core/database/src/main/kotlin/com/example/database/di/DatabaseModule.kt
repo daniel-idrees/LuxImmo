02510fe356e6557f66c4b082a5a0ee3e2b11ba93
daniyal <enggdaniyal@hotmail.com>
2026-01-15 05:47:13 +0100

    database: database module set to public for dependencies availability

--- core/database/src/main/kotlin/com/example/database/di/DatabaseModule.kt
+++ core/database/src/main/kotlin/com/example/database/di/DatabaseModule.kt
@@ -12,7 +12,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object DatabaseModule {
+object DatabaseModule {
     @Singleton
     @Provides
     fun provideAppDatabase(
