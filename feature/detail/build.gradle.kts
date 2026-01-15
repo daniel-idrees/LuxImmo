d216f87b153e6ca73c2eb8f1f9d66648357bf7a5
daniyal <enggdaniyal@hotmail.com>
2026-01-15 05:46:31 +0100

    feature detail: gradle setup with hilt and project dependencies

--- feature/detail/build.gradle.kts
+++ feature/detail/build.gradle.kts
@@ -1,13 +1,53 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+
+android {
+    namespace = "com.example.luximmo.feature.detail"
+
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures {
+        compose = true
+    }
 }
+
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_11
     }
+}
+dependencies {
+    implementation(project(":core:domain"))
+    implementation(project(":core:ui"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.android)
+
+    //hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.navigation3.ui)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //coil
+    implementation(libs.coil3.compose)
 }
