617e76da715a812f26bc251d4c6bb0d8c8540d6f
daniyal <enggdaniyal@hotmail.com>
2026-01-15 01:45:22 +0100

    database: room database config added

---
+++ core/database/src/main/kotlin/com/example/database/LuxImmoDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.database.dao.ListingDao
+import com.example.database.model.ListingEntity
+
+@Database(entities = [ListingEntity::class], version = 1, exportSchema = false)
+internal abstract class LuxImmoDatabase : RoomDatabase() {
+    abstract fun listingDao(): ListingDao
+}
