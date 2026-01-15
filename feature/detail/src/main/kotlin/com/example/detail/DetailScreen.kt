381b8e22b4aca8f499e068a53b93a85e90fa8e1b
daniyal <enggdaniyal@hotmail.com>
2026-01-15 05:50:09 +0100

    feature detail: detail screen and with viewmodel added

--- feature/detail/src/main/kotlin/com/example/detail/DetailScreen.kt
+++ feature/detail/src/main/kotlin/com/example/detail/DetailScreen.kt
@@ -1,4 +1,20 @@
 package com.example.detail
 
-class DetailScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun DetailScreen(
+    viewModel: DetailViewModel = hiltViewModel()
+) {
+
+}
+
+
+
+@Composable
+@PreviewLightDark
+private fun DetailPreview() {
+    DetailScreen()
 }
--- feature/detail/src/main/kotlin/com/example/detail/DetailViewModel.kt
+++ feature/detail/src/main/kotlin/com/example/detail/DetailViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.detail
 
-class DetailViewModel() {
+import androidx.lifecycle.ViewModel
+import com.example.domain.usecase.GetListingDetailUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel(assistedFactory = DetailViewModel.Factory::class)
+class DetailViewModel @AssistedInject constructor(
+    private val getListingDetailUseCase: GetListingDetailUseCase,
+    @Assisted val listingId: String,
+): ViewModel() {
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            listingId: String,
+        ): DetailViewModel
+    }
 }
