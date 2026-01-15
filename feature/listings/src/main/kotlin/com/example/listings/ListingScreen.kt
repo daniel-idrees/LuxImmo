9bddcdfd9c72ec01c1e71a9b7790cfe952a16612
daniyal <enggdaniyal@hotmail.com>
2026-01-15 05:50:00 +0100

    feature listings: listing screen and with viewmodel added

--- feature/listings/src/main/kotlin/com/example/listings/ListingScreen.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ListingScreen.kt
@@ -1,4 +1,17 @@
 package com.example.listings
 
-class ListingScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun ListingScreen(
+    listingViewModel: ListingViewModel = hiltViewModel()
+) {
+}
+
+@Composable
+@PreviewLightDark
+private fun ListingPreview() {
+    ListingScreen()
 }
