e5b1a6c3c034a085aae2e96f2dc3e85084163ca3
daniyal <enggdaniyal@hotmail.com>
2026-01-15 01:45:45 +0100

    database: daomodule for injecting dao

---
+++ core/database/src/main/kotlin/com/example/database/di/DaoModule.kt
@@ -0,0 +1,17 @@
+package com.example.database.di
+
+import com.example.database.LuxImmoDatabase
+import com.example.database.dao.ListingDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DaoModule {
+    @Provides
+    fun providesListingDao(
+        database: LuxImmoDatabase
+    ): ListingDao = database.listingDao()
+}
