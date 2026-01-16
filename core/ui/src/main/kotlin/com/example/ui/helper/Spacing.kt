76e96f5f48f470457aa317512b86747ec620368c
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:32:17 +0100

    ui: Spacing constants

---
+++ core/ui/src/main/kotlin/com/example/ui/helper/Spacing.kt
@@ -0,0 +1,7 @@
+package com.example.ui.helper
+
+const val SPACING_EXTRA_SMALL = 4
+const val SPACING_SMALL = 8
+const val SPACING_MEDIUM = 16
+const val SPACING_LARGE = 24
+const val SPACING_EXTRA_LARGE = 48
