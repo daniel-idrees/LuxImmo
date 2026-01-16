47491d04b856c25c01ae19cc917d9bdd1cec11a8
daniyal <enggdaniyal@hotmail.com>
2026-01-16 20:34:47 +0100

    domain: use case cleanup with no refresh call

--- core/domain/src/main/kotlin/com/example/domain/usecase/GetListingDetailUseCase.kt
+++ core/domain/src/main/kotlin/com/example/domain/usecase/GetListingDetailUseCase.kt
@@ -1,18 +1,11 @@
 package com.example.domain.usecase
 
 import com.example.domain.repository.ListingRepository
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class GetListingDetailUseCase @Inject constructor(
     private val listingsRepository: ListingRepository
 ) {
-    operator fun invoke(listingId: Int) = listingsRepository
-        .getListing(listingId)
-        .onEach {
-            if (it == null) {
-                listingsRepository.refreshListingDetails(listingId)
-            }
-        }.distinctUntilChanged()
+    operator fun invoke(listingId: Int) = listingsRepository.getListing(listingId)
+
 }
