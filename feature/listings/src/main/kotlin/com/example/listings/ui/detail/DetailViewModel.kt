a51567819283a2a6eac8a82d0a211836625c42b0
daniyal <enggdaniyal@hotmail.com>
2026-01-16 20:51:18 +0100

    feature:listing experiemntal annotation and error string from string resource

--- feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailViewModel.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailViewModel.kt
@@ -1,7 +1,10 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.example.listings.ui.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.ui.R
 import com.example.domain.usecase.GetListingDetailUseCase
 import com.example.listings.models.toListingUi
 import com.example.ui.resource.ResourceProvider
@@ -9,11 +12,13 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel(assistedFactory = DetailViewModel.Factory::class)
@@ -25,19 +30,22 @@ internal class DetailViewModel @AssistedInject constructor(
 
     val viewState: StateFlow<DetailUiState> =
         getListingDetailUseCase(listingId.toInt())
+            .onStart {
+                DetailUiState(isLoading = true)
+            }
             .distinctUntilChanged()
             .catch {
                 DetailUiState(error = it.message)
             }
             .mapLatest {
                 if (it != null) {
-                    DetailUiState(listing = it.toListingUi(resourceProvider))
+                    DetailUiState(listing = it.toListingUi(resourceProvider), isLoading = false, error = null)
                 } else {
-                    DetailUiState(error = "Listing not found")
+                    DetailUiState(error = resourceProvider.getString(R.string.error_unknown))
                 }
             }.stateIn(
                 scope = viewModelScope,
-                started = SharingStarted.Companion.WhileSubscribed(5000),
+                started = SharingStarted.WhileSubscribed(5000),
                 initialValue = DetailUiState()
             )
 
