a78eae71beb3f76b481560b591973236e7929ce7
daniyal <enggdaniyal@hotmail.com>
2026-01-16 20:33:04 +0100

    database: auto refresh for listing and result event expose

--- core/data/src/main/kotlin/com/example/data/repository/OfflineFirstListingRepository.kt
+++ core/data/src/main/kotlin/com/example/data/repository/OfflineFirstListingRepository.kt
@@ -11,7 +11,10 @@ import com.example.network.GslNetworkDataSource
 import com.example.network.model.NetworkListing
 import com.example.network.runSuspendCatching
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import java.io.IOException
 import javax.inject.Inject
 
@@ -21,13 +24,29 @@ class OfflineFirstListingRepository @Inject constructor(
     private val dao: ListingDao,
 ) : ListingRepository {
 
+    override val refreshListResultEvent : SharedFlow<Result>
+        field = MutableSharedFlow<Result>(
+            replay = 0,
+            extraBufferCapacity = 1
+        )
+
     override val listings: Flow<List<Listing>> = dao.getAllListings()
+        .onStart {
+            refreshListings()
+        }
         .map { entities ->
             entities.map(ListingEntity::asExternalModel)
         }
 
     override fun getListing(listingId: Int): Flow<Listing?> = dao.getListingById(listingId)
+        .onStart {
+            val listing = dao.getListingByIdOnce(listingId)
+            if (listing == null) {
+                refreshListingDetails(listingId)
+            }
+        }
         .map { it?.asExternalModel() }
+
 
     override suspend fun refreshListings(): Result {
         runSuspendCatching {
@@ -51,10 +70,13 @@ class OfflineFirstListingRepository @Inject constructor(
         }.fold(
             onSuccess = { networkListing ->
                 dao.upsertListing(networkListing.asEntity())
+                refreshListResultEvent.emit(Result.Success)
                 return Result.Success
             },
             onFailure = {
-                return it.toErrorResult()
+                val errorResult = it.toErrorResult()
+                refreshListResultEvent.emit(errorResult)
+                return errorResult
             }
         )
     }
