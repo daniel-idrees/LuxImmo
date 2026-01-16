ce50bd0a7fe2d6b5a3d3d98191406313129cc94d
daniyal <enggdaniyal@hotmail.com>
2026-01-16 22:21:55 +0100

    core:ui: device preview annotation defined

---
+++ core/ui/src/main/kotlin/com/example/ui/util/DevicePreviews.kt
@@ -0,0 +1,25 @@
+package com.example.ui.util
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_NO
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(
+    name = "Light Mode Phone",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    device = "spec:width=360dp,height=640dp,dpi=480",
+    backgroundColor = 0xFFFFFFFFL
+)
+@Preview(
+    name = "Dark Mode Phone",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+    device = "spec:width=360dp,height=640dp,dpi=480",
+    backgroundColor = 0xFF000000L
+)
+annotation class DevicePreviews()
+
+
