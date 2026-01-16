4dbd25dbfe320ff03c41007292405a771225d493
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:44:53 +0100

    feature:listing: detail ui state

---
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailUiState.kt
@@ -0,0 +1,9 @@
+package com.example.listings.ui.detail
+
+import com.example.listings.models.ListingUi
+
+internal data class DetailUiState (
+    val isLoading: Boolean = false,
+    val listing: ListingUi? = null,
+    val error: String? = null,
+)
