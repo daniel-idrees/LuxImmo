b8d603ba1ce5d70614239eb9c95a06f4970354d6
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:36:43 +0100

    ui: side effect helper

---
+++ core/ui/src/main/kotlin/com/example/ui/helper/SideEffectExtensions.kt
@@ -0,0 +1,28 @@
+package com.example.ui.helper
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.ui.mvi.ViewSideEffect
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+/**
+ * Collects [SideEffect] from a [Flow] in a lifecycle-aware manner.
+ *
+ * Source: https://github.com/philipplackner/CryptoTracker/blob/master/app/src/main/java/com/plcoding/cryptotracker/core/presentation/util/ObserveAsEvents.kt
+ */
+@Composable
+fun ObserveSideEffects(effect: Flow<ViewSideEffect>, onAction: (ViewSideEffect) -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                effect.collect(onAction)
+            }
+        }
+    }
+}
