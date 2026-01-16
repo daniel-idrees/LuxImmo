b3b5d1ebe0d58076ae39a2d47bc82d3b14b7491e
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:50:04 +0100

    app: NavDisplay update with removal of animations as we have only one  screen for now

--- app/src/main/java/com/example/luximmo/ui/LuxImmoApp.kt
+++ app/src/main/java/com/example/luximmo/ui/LuxImmoApp.kt
@@ -1,9 +1,5 @@
 package com.example.luximmo.ui
 
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,9 +10,8 @@ import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
-import com.example.detail.detailEntry
-import com.example.listings.ListingNavKey
-import com.example.listings.listingEntry
+import com.example.listings.navigation.ListingNavKey
+import com.example.listings.navigation.listingEntry
 import com.example.ui.navigation.Navigator
 
 
@@ -36,7 +31,6 @@ fun LuxImmoApp() {
 
         val entryProvider = entryProvider {
             listingEntry(navigator)
-            detailEntry(navigator)
         }
 
         NavDisplay(
@@ -44,36 +38,6 @@ fun LuxImmoApp() {
             onBack = { navigator.goBack() },
             modifier = Modifier.padding(innerPadding),
             entryProvider = entryProvider,
-            transitionSpec = {
-                // Slide in from right when navigating forward
-                slideInHorizontally(
-                    initialOffsetX = { it },
-                    animationSpec = tween(1000)
-                ) togetherWith slideOutHorizontally(
-                    targetOffsetX = { -it },
-                    animationSpec = tween(1000)
-                )
-            },
-            popTransitionSpec = {
-                // Slide in from left when navigating back
-                slideInHorizontally(
-                    initialOffsetX = { -it },
-                    animationSpec = tween(1000)
-                ) togetherWith slideOutHorizontally(
-                    targetOffsetX = { it },
-                    animationSpec = tween(1000)
-                )
-            },
-            predictivePopTransitionSpec = {
-                // Slide in from left when navigating back
-                slideInHorizontally(
-                    initialOffsetX = { -it },
-                    animationSpec = tween(1000)
-                ) togetherWith slideOutHorizontally(
-                    targetOffsetX = { it },
-                    animationSpec = tween(1000)
-                )
-            },
         )
     }
 }
