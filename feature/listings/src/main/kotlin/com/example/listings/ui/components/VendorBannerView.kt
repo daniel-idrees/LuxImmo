7a5955266a2b4d76d0b9e0cc183d8ac77b12cc2e
daniyal <enggdaniyal@hotmail.com>
2026-01-16 20:30:16 +0100

    feature:listing: Material color for vendor banner to support dark view

--- feature/listings/src/main/kotlin/com/example/listings/ui/components/VendorBannerView.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/components/VendorBannerView.kt
@@ -8,16 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.ui.helper.SPACING_MEDIUM
+import com.example.designsystem.util.SPACING_MEDIUM
 
 @Composable
 internal fun VendorBannerView(vendor: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.LightGray),
+            .background(MaterialTheme.colorScheme.surface),
     ) {
         Text(
             modifier = Modifier.padding(horizontal = SPACING_MEDIUM.dp),
