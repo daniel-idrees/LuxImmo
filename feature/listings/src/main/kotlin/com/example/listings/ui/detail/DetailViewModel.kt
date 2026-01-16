de03af46484d030557a3648b9bc184c578c113c0
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:45:21 +0100

    feature:listing: detail screen viewmodel setup with local data storage fetching

---
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.listings.ui.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.usecase.GetListingDetailUseCase
+import com.example.listings.models.toListingUi
+import com.example.ui.resource.ResourceProvider
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.stateIn
+
+@HiltViewModel(assistedFactory = DetailViewModel.Factory::class)
+internal class DetailViewModel @AssistedInject constructor(
+    getListingDetailUseCase: GetListingDetailUseCase,
+    private val resourceProvider: ResourceProvider,
+    @Assisted val listingId: String,
+) : ViewModel() {
+
+    val viewState: StateFlow<DetailUiState> =
+        getListingDetailUseCase(listingId.toInt())
+            .distinctUntilChanged()
+            .catch {
+                DetailUiState(error = it.message)
+            }
+            .mapLatest {
+                if (it != null) {
+                    DetailUiState(listing = it.toListingUi(resourceProvider))
+                } else {
+                    DetailUiState(error = "Listing not found")
+                }
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.Companion.WhileSubscribed(5000),
+                initialValue = DetailUiState()
+            )
+
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            listingId: String,
+        ): DetailViewModel
+    }
+}
