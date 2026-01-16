b3b25280bdd2933c42a68e4713150cf0a0e1b787
daniyal <enggdaniyal@hotmail.com>
2026-01-16 20:34:14 +0100

    domain: auto refresh result event in the use case

--- core/domain/src/main/kotlin/com/example/domain/usecase/GetListingsUseCase.kt
+++ core/domain/src/main/kotlin/com/example/domain/usecase/GetListingsUseCase.kt
@@ -1,6 +1,8 @@
 package com.example.domain.usecase
 
 import com.example.domain.repository.ListingRepository
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 
@@ -8,5 +10,8 @@ class GetListingsUseCase @Inject constructor(
     private val listingsRepository: ListingRepository
 ) {
     operator fun invoke() = listingsRepository.listings
+
     suspend fun refresh() = listingsRepository.refreshListings()
+
+    val refreshResultEvent = listingsRepository.refreshListResultEvent
 }
--- feature/listings/src/main/kotlin/com/example/listings/ui/list/ListingViewModel.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/list/ListingViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.listings.ui.list
 
 import androidx.lifecycle.viewModelScope
+import com.example.core.ui.R
 import com.example.domain.Result
 import com.example.domain.model.Listing
 import com.example.domain.usecase.GetListingsUseCase
@@ -8,9 +9,11 @@ import com.example.listings.models.toListingUi
 import com.example.ui.mvi.MviViewModel
 import com.example.ui.resource.ResourceProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,17 +24,13 @@ internal class ListingViewModel @Inject constructor(
     private val getListingsUseCase: GetListingsUseCase
 ) : MviViewModel<ListingUiAction, ListingUiState, ListingUiEffect>() {
 
-    private val listings by lazy {
-        getListingsUseCase()
-    }
-
     override fun setInitialState(): ListingUiState = ListingUiState()
 
     override fun handleAction(event: ListingUiAction) {
         when (event) {
             ListingUiAction.Init -> {
+                observeRefreshResult()
                 loadData()
-                refreshData()
             }
 
             ListingUiAction.Refresh -> {
@@ -51,7 +50,7 @@ internal class ListingViewModel @Inject constructor(
             }
 
             is ListingUiAction.OnSortChange -> {
-                setState { copy(activeSort = event.newSort, isLoading = true) }
+                setState { copy(activeSort = event.newSort) }
             }
 
             ListingUiAction.RemoveSelection -> {
@@ -64,14 +63,31 @@ internal class ListingViewModel @Inject constructor(
         }
     }
 
+    private fun observeRefreshResult() {
+        viewModelScope.launch {
+            getListingsUseCase.refreshResultEvent
+                .collectLatest {
+                    when (it) {
+                        is Result.Error -> handleErrorForSilentSync(it)
+                        is Result.Success -> {
+                            // notify user that data is latest
+                        }
+                    }
+                }
+        }
+    }
+
     private fun loadData() {
 
         val sortOptionFlow = viewState
             .mapLatest { it.activeSort }
             .distinctUntilChanged()
+            .onEach {
+                setState { copy(isLoading = true) }
+            }
 
         viewModelScope.launch {
-            listings
+            getListingsUseCase()
                 .onStart {
                     setState { copy(isLoading = true) }
                 }
@@ -99,35 +115,75 @@ internal class ListingViewModel @Inject constructor(
         when (sortOption) {
             ListingSortOption.Default -> this
             ListingSortOption.PriceAsc -> this.sortedBy { it.price }
-            ListingSortOption.PriceDesc -> this.sortedByDescending { it.price}
+            ListingSortOption.PriceDesc -> this.sortedByDescending { it.price }
             ListingSortOption.Area -> this.sortedBy { it.area }
         }
 
-
+    /**
+     * For explicit data refresh
+     */
     private fun refreshData() {
         viewModelScope.launch {
             val result = getListingsUseCase.refresh()
             when (result) {
-                is Result.Error -> {
-                    if (viewState.value.listings.isEmpty()) {
-                        setState {
-                            copy(
-                                isLoading = false,
-                                error = "Something went wrong"   //handle inter error
-                            )
-                        }
-                    } else {
-                        setEffect {
-                            ListingUiEffect.ShowSnackbar(
-                                message = "Something went wrong"
-                            )
-                        }
-                    }
-                }
-
+                is Result.Error -> handleErrorForManualSync(result)
                 Result.Success -> {
                     setState { copy(isLoading = false) }
                 }
+            }
+        }
+    }
+
+    private fun handleErrorForManualSync(error: Result.Error) {
+        when (error) {
+            Result.Error.NoInternetConnection -> handleNoInternetError()
+            Result.Error.Unknown -> {
+                if (viewState.value.listings.isEmpty()) {
+                    setState {
+                        copy(
+                            isLoading = false,
+                            error = resourceProvider.getString(R.string.error_unknown)
+                        )
+                    }
+                } else {
+                    setEffect {
+                        ListingUiEffect.ShowSnackbar(
+                            message = resourceProvider.getString(R.string.error_unknown)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleErrorForSilentSync(error: Result.Error) {
+        when (error) {
+            is Result.Error.Unknown -> {
+                if (viewState.value.listings.isEmpty()) {
+                    setState {
+                        copy(
+                            error = resourceProvider.getString(R.string.error_unknown)
+                        )
+                    }
+                }
+            }
+
+            is Result.Error.NoInternetConnection -> handleNoInternetError()
+        }
+    }
+
+    private fun handleNoInternetError() {
+        setEffect {
+            ListingUiEffect.ShowSnackbar(
+                message = resourceProvider.getString(R.string.error_not_internet)
+            )
+        }
+
+        if (viewState.value.listings.isEmpty()) {
+            setState {
+                copy(
+                    error = resourceProvider.getString(R.string.error_not_internet)
+                )
             }
         }
     }
