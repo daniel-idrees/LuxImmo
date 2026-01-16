5753e9ae775e6729d750c6b5ae4315076bdaae03
daniyal <enggdaniyal@hotmail.com>
2026-01-16 03:48:16 +0100

    feature:listing: listing detail pane view added in the listing entry provider

--- feature/listings/src/main/kotlin/com/example/listings/navigation/ListingEntryProvider.kt
+++ feature/listings/src/main/kotlin/com/example/listings/navigation/ListingEntryProvider.kt
@@ -1,11 +1,12 @@
-package com.example.listings
+package com.example.listings.navigation
 
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
+import com.example.listings.ui.AdaptiveListingsDetailPane
 import com.example.ui.navigation.Navigator
 
 fun EntryProviderScope<NavKey>.listingEntry(navigator: Navigator) {
     entry<ListingNavKey> {
-        ListingScreen() //TODO add navigation to detail
+        AdaptiveListingsDetailPane()
     }
 }
