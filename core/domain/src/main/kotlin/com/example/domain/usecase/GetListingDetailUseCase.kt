a2a8f1ab770a547d43b709f506c736e37b7095a9
daniyal <enggdaniyal@hotmail.com>
2026-01-16 04:30:58 +0100

    ui: refresh listing handling when locally not available

--- core/domain/src/main/kotlin/com/example/domain/usecase/GetListingDetailUseCase.kt
+++ core/domain/src/main/kotlin/com/example/domain/usecase/GetListingDetailUseCase.kt
@@ -1,11 +1,18 @@
 package com.example.domain.usecase
 
 import com.example.domain.repository.ListingRepository
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class GetListingDetailUseCase @Inject constructor(
     private val listingsRepository: ListingRepository
 ) {
-    operator fun invoke(listingId: Int) = listingsRepository.getListing(listingId)
-    suspend fun refresh(listingId: Int) = listingsRepository.refreshListingDetails(listingId)
+    operator fun invoke(listingId: Int) = listingsRepository
+        .getListing(listingId)
+        .onEach {
+            if (it == null) {
+                listingsRepository.refreshListingDetails(listingId)
+            }
+        }.distinctUntilChanged()
 }
