ca889abf2eaf1492472bc071fb8eee8e8cefddf6
daniyal <enggdaniyal@hotmail.com>
2026-01-17 19:14:04 +0100

    core:domain: throwable extension update after result model update

--- core/network/src/main/kotlin/com/example/network/ThrowableExt.kt
+++ core/network/src/main/kotlin/com/example/network/ThrowableExt.kt
@@ -1,10 +1,11 @@
 package com.example.network
 
+import com.example.domain.AppError
 import com.example.domain.Result
 import java.io.IOException
 
 fun Throwable.toErrorResult(): Result.Error =
     when (this) {
-        is IOException -> Result.Error.NoInternetConnection
-        else -> Result.Error.Unknown
+        is IOException -> Result.Error(error = AppError.NoInternetConnection)
+        else -> Result.Error(error = AppError.Unknown)
     }
