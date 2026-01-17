3543ed50f9c3599ac8d586aea01608c64335ccc3
daniyal <enggdaniyal@hotmail.com>
2026-01-17 19:12:09 +0100

    core:database: visibility set to internal for modules

--- core/database/src/main/kotlin/com/example/database/LuxImmoDatabase.kt
+++ core/database/src/main/kotlin/com/example/database/LuxImmoDatabase.kt
@@ -6,6 +6,6 @@ import com.example.database.dao.ListingDao
 import com.example.database.model.ListingEntity
 
 @Database(entities = [ListingEntity::class], version = 1, exportSchema = false)
-abstract class LuxImmoDatabase : RoomDatabase() {
+internal abstract class LuxImmoDatabase : RoomDatabase() {
     abstract fun listingDao(): ListingDao
 }
