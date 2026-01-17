a2f5963d3a4fdfe051e1926f7f21c349211c4ee9
daniyal <enggdaniyal@hotmail.com>
2026-01-17 19:27:38 +0100

    feature:listings: detail ui state set to immutable

--- feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailUiState.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailUiState.kt
@@ -1,7 +1,9 @@
 package com.example.listings.ui.detail
 
+import androidx.compose.runtime.Immutable
 import com.example.listings.models.ListingUi
 
+@Immutable
 internal data class DetailUiState(
     val isLoading: Boolean = false,
     val listing: ListingUi? = null,
