28ec46109c1256c020f4908a82e60d121bf8e555
daniyal <enggdaniyal@hotmail.com>
2026-01-17 03:51:16 +0100

    feature:listings: lint fix

--- feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailUiState.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailUiState.kt
@@ -2,7 +2,7 @@ package com.example.listings.ui.detail
 
 import com.example.listings.models.ListingUi
 
-internal data class DetailUiState (
+internal data class DetailUiState(
     val isLoading: Boolean = false,
     val listing: ListingUi? = null,
     val error: String? = null,
