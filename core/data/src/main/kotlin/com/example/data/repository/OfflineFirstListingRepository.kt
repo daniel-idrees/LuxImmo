cc18c7c5e54c3ab9765bf5fefe784d49c14eaaef
daniyal <enggdaniyal@hotmail.com>
2026-01-18 16:47:54 +0100

    core:data data fetch and refresh seperated in the repo implementation

--- core/data/src/main/kotlin/com/example/data/repository/OfflineFirstListingRepository.kt
+++ core/data/src/main/kotlin/com/example/data/repository/OfflineFirstListingRepository.kt
@@ -5,8 +5,6 @@ import com.example.data.model.asExternalModel
 import com.example.database.dao.ListingDao
 import com.example.database.model.ListingEntity
 import com.example.domain.Result
-import com.example.domain.exception.ListingDetailUnavailableException
-import com.example.domain.exception.ListingsUnavailableException
 import com.example.domain.model.Listing
 import com.example.domain.repository.ListingRepository
 import com.example.network.GslNetworkDataSource
@@ -15,8 +13,6 @@ import com.example.network.runSuspendCatching
 import com.example.network.toErrorResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 internal class OfflineFirstListingRepository @Inject constructor(
@@ -24,29 +20,15 @@ internal class OfflineFirstListingRepository @Inject constructor(
     private val dao: ListingDao,
 ) : ListingRepository {
 
-    override fun observeListings(): Flow<List<Listing>> = dao.getAllListings()
-        .onStart {
-            val result = refreshListings()
-            if (result is Result.Error) {
-                throw ListingsUnavailableException(result)
+    override val listings: Flow<List<Listing>> =
+        dao.getAllListings()
+            .map { entities ->
+                entities.map(ListingEntity::asExternalModel)
             }
-        }
-        .map { entities ->
-            entities.map(ListingEntity::asExternalModel)
-        }
 
-    override fun getListing(listingId: Int): Flow<Listing> = dao.getListingById(listingId)
-        .transform { entity ->
-            if (entity == null) {
-                val result = refreshListingDetails(listingId)
-                if (result is Result.Error) {
-                    throw ListingDetailUnavailableException(result)
-                }
-            } else {
-                emit(entity)
-            }
-        }
-        .map { it.asExternalModel() }
+    override fun getListing(listingId: Int): Flow<Listing?> =
+        dao.getListingById(listingId)
+            .map { it?.asExternalModel() }
 
     override suspend fun refreshListings(): Result<List<Listing>> {
         runSuspendCatching {
@@ -66,14 +48,14 @@ internal class OfflineFirstListingRepository @Inject constructor(
         )
     }
 
-    private suspend fun refreshListingDetails(listingId: Int): Result<ListingEntity> {
+    override suspend fun refreshListing(listingId: Int): Result<Listing> {
         runSuspendCatching {
             gslNetworkDataSource.getListing(listingId)
         }.fold(
             onSuccess = { networkListing ->
                 val entity = networkListing.asEntity()
                 dao.upsertListing(entity)
-                return Result.Success(entity)
+                return Result.Success(entity.asExternalModel())
             },
             onFailure = {
                 return it.toErrorResult()
