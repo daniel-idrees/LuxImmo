9faa2d86118666e04504910f81856cd586309e4b
daniyal <enggdaniyal@hotmail.com>
2026-01-18 16:14:30 +0100

    core:common module added

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.example.luximmo.core.common"
+
+    compileSdk {
+        version = release(36)
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
+
+dependencies {
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
