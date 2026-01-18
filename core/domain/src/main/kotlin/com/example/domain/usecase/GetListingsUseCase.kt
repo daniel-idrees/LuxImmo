d91a18de8a7f27aa16535f2cfae3c2093c18d66a
daniyal <enggdaniyal@hotmail.com>
2026-01-18 16:48:14 +0100

    core:domain use case update after repository change

--- core/domain/src/main/kotlin/com/example/domain/usecase/GetListingsUseCase.kt
+++ core/domain/src/main/kotlin/com/example/domain/usecase/GetListingsUseCase.kt
@@ -3,11 +3,9 @@ package com.example.domain.usecase
 import com.example.domain.repository.ListingRepository
 import javax.inject.Inject
 
-
 class GetListingsUseCase @Inject constructor(
     private val listingsRepository: ListingRepository
 ) {
-    operator fun invoke() = listingsRepository.observeListings()
-
+    operator fun invoke() = listingsRepository.listings
     suspend fun refresh() = listingsRepository.refreshListings()
 }
