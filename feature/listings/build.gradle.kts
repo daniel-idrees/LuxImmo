604527c131803de648d0778211e6059fe8ff56fb
daniyal <enggdaniyal@hotmail.com>
2026-01-18 16:16:39 +0100

    feature:listing dependency on common module added

--- feature/listings/build.gradle.kts
+++ feature/listings/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
 dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:ui"))
+    implementation(project(":core:common"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.android)
