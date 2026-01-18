1fe23a55ff90709667ef2d7bb77e05e01626d881
daniyal <enggdaniyal@hotmail.com>
2026-01-18 16:28:32 +0100

    feature:listing improved data handling as offline first and error handling

--- feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailViewModel.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/detail/DetailViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.core.ui.R
 import com.example.domain.AppError
-import com.example.domain.exception.ListingDetailUnavailableException
+import com.example.domain.Result
+import com.example.domain.model.Listing
 import com.example.domain.usecase.GetListingDetailUseCase
 import com.example.listings.models.toListingUi
+import com.example.ui.models.UiErrorConfig
 import com.example.ui.resource.ResourceProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -16,43 +18,87 @@ import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 @HiltViewModel(assistedFactory = DetailViewModel.Factory::class)
 internal class DetailViewModel @AssistedInject constructor(
-    getListingDetailUseCase: GetListingDetailUseCase,
+    private val getListingDetailUseCase: GetListingDetailUseCase,
     private val resourceProvider: ResourceProvider,
     @Assisted val listingId: Int,
 ) : ViewModel() {
     private val _error = Channel<String>(capacity = Channel.BUFFERED)
     val errorEffect = _error.receiveAsFlow()
 
+    private val syncResult = MutableStateFlow<Result<Listing>?>(null)
     val viewState: StateFlow<DetailUiState> =
-        getListingDetailUseCase(listingId)
-            .distinctUntilChanged()
-            .mapLatest {
-                DetailUiState(listing = it.toListingUi(resourceProvider), isLoading = false)
-            }
-            .catch {
-                val errorMessage =
-                    if (it is ListingDetailUnavailableException && it.errorResult.error is AppError.NoInternetConnection) {
-                        resourceProvider.getString(R.string.error_not_internet)
-                    } else {
-                        resourceProvider.getString(R.string.error_unknown)
+        combine(
+            getListingDetailUseCase(listingId),
+            syncResult
+        ) { listing, result ->
+            when {
+                listing != null -> {
+                    when {
+                        result is Result.Error -> {
+                            //TODO think something how to show user that data is cached
+                        }
+
+                        result is Result.Success -> {
+                            //TODO think something remove any cached indication
+                        }
                     }
-                _error.send(errorMessage)
+
+                    DetailUiState(
+                        listing = listing.toListingUi(resourceProvider),
+                        isLoading = false
+                    )
+                }
+
+                result is Result.Error -> {
+                    DetailUiState(
+                        isLoading = false,
+                        error = UiErrorConfig(
+                            getErrorTextForErrorResult(result),
+                            onRetry = ::refreshListing
+                        )
+                    )
+                }
+
+                else -> {
+                    DetailUiState(isLoading = true) // keep waiting for the data
+                }
             }
+        }
+            .onStart {
+                refreshListing()
+            }
+            .distinctUntilChanged()
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(5000),
                 initialValue = DetailUiState(isLoading = true)
             )
+
+    private fun refreshListing() {
+        viewModelScope.launch {
+            val result = getListingDetailUseCase.refresh(listingId)
+            syncResult.emit(result)
+        }
+    }
+
+    private fun getErrorTextForErrorResult(result: Result.Error): String {
+        return when (result.error) {
+            AppError.NoInternetConnection -> resourceProvider.getString(R.string.error_offline)
+            AppError.Unknown -> resourceProvider.getString(R.string.error_unknown)
+        }
+    }
 
     @AssistedFactory
     interface Factory {
