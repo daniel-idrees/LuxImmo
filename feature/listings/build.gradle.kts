54402d8993e3ed6c84fc47b5d7121eda618a0724
daniyal <enggdaniyal@hotmail.com>
2026-01-19 21:03:42 +0100

    core:ui test dependencies in gradle

--- feature/listings/build.gradle.kts
+++ feature/listings/build.gradle.kts
@@ -6,13 +6,20 @@ plugins {
 
 android {
     namespace = "com.example.luximmo.feature.listings"
+    testOptions.unitTests.isIncludeAndroidResources = true
 }
 
 dependencies {
     //testing
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+    testImplementation (libs.mockito.kotlin)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    testImplementation(kotlin("test"))
 }
