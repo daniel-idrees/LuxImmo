7aeee0c9c5d129cff3302ccbe88f127481d2dc74
daniyal <enggdaniyal@hotmail.com>
2026-01-19 21:10:18 +0100

    feature:listing main dispatcher rule added to support testing

---
+++ feature/listings/src/test/java/com/example/listings/util/MainDispatcherRule.kt
@@ -0,0 +1,26 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.example.listings.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+// TODO: to move to a common test module so that other features can also use it
+
+class MainDispatcherRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
