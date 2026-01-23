cbfe48477f2016a7a5b5a9a694a780aa922d22b7
daniyal <enggdaniyal@hotmail.com>
2026-01-23 18:46:09 +0100

    feature:listings: dynamic async image used for listing image

--- feature/listings/src/main/kotlin/com/example/listings/ui/components/ListingImageView.kt
+++ feature/listings/src/main/kotlin/com/example/listings/ui/components/ListingImageView.kt
@@ -1,13 +1,9 @@
 package com.example.listings.ui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import coil3.compose.AsyncImage
+import com.example.ui.components.DynamicAsyncImage
 
 @Composable
 internal fun ListingImageView(
@@ -15,16 +11,10 @@ internal fun ListingImageView(
     imageUrl: String?,
     contentScale: ContentScale = ContentScale.Fit,
 ) {
-    Box(
-        modifier = modifier
-            .background(Color.LightGray)
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxSize(),
-            model = imageUrl,
-            contentDescription = "Listing Image",
-            contentScale = contentScale,
-        )
-    }
+    DynamicAsyncImage(
+        imageUrl = imageUrl,
+        contentDescription = "Listing Image",
+        modifier = modifier,
+        contentScale = contentScale
+    )
 }
